13e087b770957a2b770f0fe27e3dba3d4e073f27
felipe-olegario <felipe.olegario@bandtec.com.br>
2023-10-25 09:50:03 -0300

    feat: implements tanstack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './views/Home/Home.jsx';
-import Navbar from './components/Navbar.jsx';
 import Detail from './views/Detail/Detail.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 30,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,13 +24,18 @@ const router = createBrowserRouter([
   },
   {
     path: "/detail/:movieId",
-    element: <Detail/>,
+    element: (
+      <>
+        <Detail/>
+      </>
+    ),
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <Navbar/>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </>
 )
